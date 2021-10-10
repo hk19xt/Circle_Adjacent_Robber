5efdda0eacbd12ec4b74bab261e5cea655f89ec0
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-10-10 16:03:28 -0400

    Update Main.java

--- Circle_Robber/src/Circle_Robber/Main.java
+++ Circle_Robber/src/Circle_Robber/Main.java
@@ -2,6 +2,10 @@ package Circle_Robber;
 
 public class Main {
     public static void main(String[] args){
-        
+        Circle_Robber CR = new Circle_Robber();
+        int[] user_houses = {1,2,3,1};
+        int output;
+        output = CR.circle_robber(user_houses);
+        System.out.println(output);
     }
 }
