171c7db930318db65dea0a2d1595fdbfae88cc43
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-10-12 17:14:53 -0400

    Update Main.java

--- Circle_Robber/src/Circle_Robber/Main.java
+++ Circle_Robber/src/Circle_Robber/Main.java
@@ -1,11 +1,18 @@
 package Circle_Robber;
 
+/*This is the main class.
+The users can change the input value in the hardcoded line.
+The calculation will be conducted by the implementation class. The implementation will be recursively checked through for loop. 
+
+Name: Hyejin Kim
+Date: 10/12/2021*/
+
 public class Main {
     public static void main(String[] args){
-        Circle_Robber CR = new Circle_Robber();
-        int[] user_houses = {1,2,3,1};
+        Circle_Robber CR = new Circle_Robber(); 
+        int[] user_houses = {1,1,2,3}; //The users can change their input values in the array here.
         int output;
         output = CR.circle_robber(user_houses);
-        System.out.println(output);
+        System.out.println("The maximum amount of the array will be " + output);
     }
 }
